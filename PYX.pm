da229a4d4ea2d9edb13929b4f8d5bac179bdc00a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-05-11 15:29:07 +0200

    Update author name

--- PYX.pm
+++ PYX.pm
@@ -267,11 +267,11 @@ Install the Tags modules.
 
 =head1 AUTHOR
 
-Michal Špaček L<skim@cpan.org>
+Michal Josef Špaček L<skim@cpan.org>
 
 =head1 LICENSE AND COPYRIGHT
 
-© 2011-2020 Michal Špaček
+© 2011-2020 Michal Josef Špaček
 
 BSD 2-Clause License
 
